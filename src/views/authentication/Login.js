86e69aa3067e1373225fbf273878b54bc0d8c32e
Prashant Vekariya <prashantvekariya321@gmail.com>
2022-03-29 20:28:51 +0530

    build changes

--- src/views/authentication/Login.js
+++ src/views/authentication/Login.js
@@ -51,6 +51,7 @@ const Login = props => {
             ...res.data.data
           }
           dispatch(handleLogin(data))
+          window.location.reload()
           // console.log('fghjk')
           // history.push(getHomeRouteForLoggedInUser(data.data.roletype))
         })
